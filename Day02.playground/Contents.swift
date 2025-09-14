213ef103e15ae1beb7049b31de6989bbcae07a64
yomnaahk <yomnakerir@gmail.com>
2025-09-14 17:54:19 +0300

    feat: complete  Day 2 code – Booleans, string interpolation, checkpoint 1

--- Day02.playground/Contents.swift
+++ Day02.playground/Contents.swift
@@ -1,3 +1,44 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let goodDogs = true
+var gameOver = false
+print(gameOver)  //false
+gameOver.toggle()
+print(gameOver)  // true
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)  //true
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)   //false
+
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+var greeting = firstPart + secondPart
+print(greeting)
+
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+print(luggageCode)
+
+let name = "Huda"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+let missionMessage = "Apollo " + String(number) + " landed on the moon."
+print(missionMessage)
+
+print("5 x 5 is \(5 * 5)")
+
+let temperatureInCelsius = 28
+let temperatureInFahrenheit = (Float(temperatureInCelsius) * 9.0 / 5.0) + 32.0
+print("\(temperatureInCelsius) degrees Celsius is equal to \(temperatureInFahrenheit) degrees Fahrenheit.")
