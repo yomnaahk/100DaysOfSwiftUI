26149daaf85f88484e1d4cd0cdac395027b5ea9d
yomnaahk <yomnakerir@gmail.com>
2025-09-16 11:08:38 +0300

    feat: add Day 5 – If, switch, and the ternary operator

---
+++ Day05.playground/Contents.swift
@@ -0,0 +1,183 @@
+import Cocoa
+
+let score = 85
+if score > 80 {
+    print("You passed!")
+}
+
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print("Where we're going we don't need roads.")
+}
+
+if percentage < 85 {
+    print("Sorry, you failed the test.")
+}
+
+if age >= 18 {
+    print("You're eligible to vote")
+}else {
+    print("Sorry, you're too young to vote.")
+}
+
+
+let ourName = "Yomna"
+let friendName = "Huda"
+
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
+
+var numbers = [1, 3, 4, 6]
+numbers.append(10)
+print("numbers : " , numbers)
+if numbers.count > 3{
+    numbers.remove(at: 0)
+}
+print("numbers after remove first number : " , numbers)
+
+let country = "Canada"
+
+if country == "Australia" {
+    print("G'day!")
+}
+
+let name = "Taylor Swift"
+
+if name != "Anonymous" {
+    print("Welcome, \(name)")
+}
+
+var username = "taylorswift13"
+
+if username == "" {
+    username = "Anonymous"
+}
+if username.count == 0 {
+    username = "Anonymous"
+}
+if username.isEmpty == true {
+    username = "Anonymous"
+}
+if username.isEmpty {
+    username = "Anonymous"
+}
+print("Welcome, \(username)!")
+
+let a = false
+let b = true
+
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+let temp = 25
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella.")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled.")
+case .unknown:
+    print("Our forecast generator is broken!")
+default:
+    print("NO information available")
+}
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You're Batman!")
+case "Mega-City One":
+    print("You're Judge Dredd!")
+case "Wakanda":
+    print("You're Black Panther!")
+default:
+    print("Who are you?")
+}
+
+let day = 5
+print("My true love gave to me…")
+
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hens")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
+
+let user_age = 20
+let canVoted = user_age >= 18 ? "Yes" : "No"
+print(canVoted)
+
+let hour = 18
+print(hour < 12 ? "it's before noon":"it's after noon")
+
+let names = ["Jayne", "Kaylee", "Mal"]
+let crewCount = names.isEmpty ? "No one" : "\(names.count) people"
+print(crewCount)
+
+enum Theme {
+    case light, dark
+}
+
+let theme = Theme.dark
+let background = theme == .dark ? "black" : "white"
+print(background)
